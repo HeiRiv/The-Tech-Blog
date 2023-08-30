61183d584aaa9b8c652eb60a1b8c3c18c3cf478e
Heidy Beltran <HeidyBeltran@Alains-MacBook-Air.local>
2023-08-30 14:51:36 -0400

    Fixed models, seeds, controllers and views

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,16 +3,8 @@ module.exports = {
     // Format date as MM/DD/YYYY
     return date.toLocaleDateString();
   },
-  format_amount: (amount) => {
-    // format large numbers with commas
-    return parseInt(amount).toLocaleString();
-  },
-  get_emoji: () => {
-    const randomNum = Math.random();
 
-    // Return a random emoji
-    if (randomNum > 0.7) {
-      return `<span for="img" aria-label="laptop">💻</span>`;
-    }
+  get_emoji: () => {
+    return `<span for="img" aria-label="laptop">💻</span>`;
   },
 };
